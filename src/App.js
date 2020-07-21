9a1c9947a37a9370f1ffde2d7ff0c4ef78613b1c
DIKI SUPRIADI <dikisupriadi021@email.unikom.ac.id>
2020-07-21 21:05:25 +0700

    with login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Test from './component/stepper/index'
-import Table from './component/tabel/index'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./component/login/index";
+import SignUp from "./component/signup/index";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-        <Table />
-        <Test>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" >Analisis Korelasi Tanah Airku</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-        </Test>
-    </div>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
